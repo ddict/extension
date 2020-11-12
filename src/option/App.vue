a570dac587aa1dd6989ac471d4b9efcabf642c1c
ddo <joeddo89@gmail.com>
2020-11-12 22:22:34 +0700

    init option

---
+++ src/option/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="text-left">
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="icon" />
+            <label class="form-check-label" for="icon">
+                <img src="/logo/16.png" />
+                Icon
+            </label>
+            <small class="form-text text-muted">
+                There will be a Ddict Icon when you select any words.
+            </small>
+        </div>
+
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="dbclick" />
+            <label class="form-check-label" for="dbclick">
+                Double click on words to translate
+            </label>
+        </div>
+
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="shift" />
+            <label class="form-check-label" for="shift">
+                Highlight text then press "Shift" button to translate
+            </label>
+        </div>
+
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="auto_tts" />
+            <label class="form-check-label" for="auto_tts">
+                Auto Speak/TTS
+            </label>
+        </div>
+
+        <br />
+
+        <button
+            class="btn btn-lg btn-secondary btn-block"
+            @click="window.close()"
+        >
+            Close
+        </button>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            user: null,
+        }
+    },
+}
+</script>
