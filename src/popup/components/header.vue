74f512e9b07568af9d4764e9f2073c0353e89954
ddo <joeddo89@gmail.com>
2020-11-24 10:46:04 +0700

    add option page link in popup

--- src/popup/components/header.vue
+++ src/popup/components/header.vue
@@ -1,11 +1,30 @@
 <template>
-    <header class="navbar justify-content-center">
-        <a class="navbar-brand text-white"
-           href="/">
+    <header class="navbar">
+        <a class="navbar-brand text-white" href="/">
             Ddict
         </a>
+        <form class="form-inline">
+            <a :href="option_url" target="_blank">
+                ⚙️
+            </a>
+        </form>
     </header>
 </template>
+
+<script>
+const helper = require('../../helper')
+
+module.exports = {
+    data() {
+        return {
+            option_url: '',
+        }
+    },
+    created() {
+        this.option_url = helper.getURL('/option/index.html')
+    },
+}
+</script>
 
 <style>
 header {
