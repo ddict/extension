77c994147a4f57dc1a5eaaf2f41fcefdc67ba6e7
ddo <joeddo89@gmail.com>
2020-11-27 11:55:05 +0700

    get url directly from google not blob

--- src/google/index.js
+++ src/google/index.js
@@ -68,15 +68,18 @@ async function translate(lang = 'en', text, src, target) {
 
 async function tts(lang = 'en', text, target) {
     const rq = Translate.google.tts(lang, text, 'input', target)
+    return rq.url
 
+    // enable below if google require their header for tts
+    
     // to tell extension header listener to enable the custom user-agent
-    rq.headers.ddict = DDICT_HEADER
+    // rq.headers.ddict = DDICT_HEADER
 
-    const res = await fetch(rq.url, {
-        method: rq.method,
-        headers: rq.headers,
-    })
+    // const res = await fetch(rq.url, {
+    //     method: rq.method,
+    //     headers: rq.headers,
+    // })
 
-    const data = await res.blob()
-    return URL.createObjectURL(data)
+    // const data = await res.blob()
+    // return URL.createObjectURL(data)
 }
